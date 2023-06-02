81a741c541533035de2bff6c761a509ec80c6c66
lolaias <iasmyn.jeje06@gmail>
2023-06-02 12:37:25 -0300

    iasmyn pereira

---
+++ script.js
@@ -0,0 +1,44 @@
+const listElement = document.querySelector("#app ul")
+const inputElement = document.querySelector("#app input")
+const buttonElement = document.querySelector("#app button")
+
+//const tarefas = ["Estudar para prova", "Dormir cedo"]
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [] 
+
+function renderTarefas() {
+    listElement.innerHTML = ""
+    // console.log(tarefas)
+    for (const itemTarefa of tarefas) {
+        // console.log(itemTarefa)
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(itemTarefa)
+        const linkElement = document.createElement("a") 
+        linkElement.setAttribute("href", "#") 
+        const pos = tarefas.indexOf(itemTarefa)
+        linkElement.setAttribute("onclick", `deleteTarefa(${pos})`) 
+        const linkText = document.createTextNode("Excluir") 
+        linkElement.appendChild(linkText)
+
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+    }
+}
+renderTarefas()
+function addTarefas() {
+    const text = inputElement.value
+    tarefas.push(text)
+    // console.log(text)
+    renderTarefas()
+    inputElement.value = "" 
+    saveToStorage()
+}
+buttonElement.onclick = addTarefas
+function deleteTarefa(pos) { 
+    tarefas.splice(pos, 1)
+    renderTarefas()
+    saveToStorage()
+}
+function saveToStorage() {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+}
